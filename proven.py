81c2e0bfe4e2a54eaaf541f6f8f4dd5720c518bc
Atreya <agkatreya@gmail.com>
2020-05-02 15:01:18 +0530

    more exploration

--- proven.py
+++ proven.py
@@ -9,19 +9,33 @@ else:
     print("Usage: %s username" % (sys.argv[0],))
     sys.exit()
 
-token = util.prompt_for_user_token(username, secrets.scopes, client_id=secrets.client_id, client_secret=secrets.client_secret, redirect_uri=secrets.redirect_uri)
+token = util.prompt_for_user_token(username, secrets.scopes_all, client_id=secrets.cli_id, client_secret=secrets.cli_secret, redirect_uri=secrets.redir_uri)
 
 if token:
     try:
         sp = spotipy.Spotify(auth=token)
+    #    ============== To get all saved tracks =============================
     #    results = sp.current_user_saved_tracks()
     #    for item in results['items']:
     #        track = item['track']
     #        print(track['name'] + ' - ' + track['artists'][0]['name'])
-        results = sp.devices()
-        for item in results['devices']: 
-            print(item.get('name'), item.get('type'), item['id'])
-        sp.transfer_playback(secrets.device)
+    #    ====================================================================
+    #   ============== To show all devices ( and transfer playback to stored id ) ================    
+    #   +++++++++ TODO : Add menu to select device to transfer to ++++++++++++++++++++++++++++++++
+    #     print("Devices: ")
+    #     results = sp.devices()
+    #     for item in results['devices']: 
+    #         print(item.get('name'), item.get('type'), item['id'])
+    #     sp.transfer_playback(secrets.dev_derputer)
+    #   ==========================================================================================
+    #
+    #   ================= To show all saved albums ==================================================
+    #     results = sp.current_user_saved_albums()['items']
+    #     for al in results:
+    #         print(al['album']['name'], list(artist['name'] for artist in al['album']['artists']))
+    #   =============================================================================================
+    #    Shows details of an album. Turns out various artists is listed on the album level
+        print(sp.album('spotify:album:4FZ9s0pelFSliPWhVEWRcC')['artists'])
     except Exception as e:
         print(e)
 else:
